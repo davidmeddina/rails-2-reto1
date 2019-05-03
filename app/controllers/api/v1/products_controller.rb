6b6d291499a0eb7bb2cf4698e84bebf9a1473244
david medina <davidmeddina@gmail.com>
2019-05-02 21:15:49 -0500

    render json two

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,24 +1,25 @@
 class Api::V1::ProductsController < ApplicationController
+  
   def index
     @products = Product.all
     render json: @products
   end
 
-  def new
-    @product = Product.new
-  end
+  # def new
+  #   @product = Product.new
+  # end
 
-  def create
-    @product = Product.create(product_params)
-  end
+  # def create
+  #   @product = Product.create(product_params)
+  # end
 
-  def edit
-    @product = Product.find(params[:id])
-  end
+  # def edit
+  #   @product = Product.find(params[:id])
+  # end
 
-  def update
-    @product = Product.update(params[:id], product_params)
-  end
+  # def update
+  #   @product = Product.update(params[:id], product_params)
+  # end
 
   private
     def product_params
